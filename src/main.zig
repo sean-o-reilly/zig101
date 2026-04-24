805c338576ca86db7fdaefc6c5826d534d6f8274
sean-o-reilly <oreilly.e.sean@gmail.com>
2026-04-24 14:43:54 -0500

    feat: background thread to reset ball, move draw calls, randomize y velocity

--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,7 @@ const screenHeight = 1000;
 const paddleOffset = 40;
 const paddleWidth = 30;
 const paddleHeight = 150;
-const paddleMoveDistance = 15;
+const paddleMoveDistance = 20;
 
 var leftPaddle: rl.Rectangle = .{.x = paddleOffset, .y = paddleOffset, .width = paddleWidth, .height = paddleHeight};
 var rightPaddle: rl.Rectangle = .{
@@ -20,8 +20,8 @@ var rightPaddle: rl.Rectangle = .{
 var leftPaddleMoveDir : f32 = 1.0;
 var rightPaddleMoveDir : f32 = 1.0;
 
-var ball : rl.Rectangle = .{.x = screenWidth / 2, .y = screenHeight / 2, .width = ballSize, .height = ballSize};
 const ballSize = 30;
+var ball : rl.Rectangle = .{.x = screenWidth / 2, .y = screenHeight / 2, .width = ballSize, .height = ballSize};
 
 var ballXVelo : f32 = 17.0;
 var ballYVelo : f32 = 1.0;
@@ -31,6 +31,8 @@ var ballInPlay = true;
 var leftPlayerScore : u32 = 0;
 var rightPlayerScore : u32 = 0;
 
+var rand : f32 = 0.0;
+
 pub fn main() anyerror!void {
     rl.initWindow(screenWidth, screenHeight, "Pong");
     defer rl.closeWindow(); // Close window and OpenGL context
@@ -38,45 +40,48 @@ pub fn main() anyerror!void {
     rl.setTargetFPS(60);
 
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
-        rl.beginDrawing();
-        defer rl.endDrawing();
-
-        rl.clearBackground(.black);
-
-        rl.drawRectangleLines(0, 0, screenWidth, screenHeight, .blue);
-
         updatePaddles();
 
         if (ballInPlay) {
             ball.x += ballXVelo;
 
+            // ball hits left or right paddle
             if (AABB(ball, leftPaddle)) {
+                ball.x = leftPaddle.x + leftPaddle.width + 1;
                 ballXVelo *= -1;
-                ballYVelo = leftPaddleMoveDir * 4.0;
+                ballYVelo = leftPaddleMoveDir * lazyRand(4.0, 10.0);
             }
             else if (AABB(ball, rightPaddle)) {
+                ball.x = rightPaddle.x - 1 - ball.width;
                 ballXVelo *= -1;
-                ballYVelo = rightPaddleMoveDir * 4.0;
+                ballYVelo = rightPaddleMoveDir * lazyRand(4.0, 10.0);
             }
 
             ball.y += ballYVelo;
 
-            if (ball.y < 0 or ball.y + ball.height > screenHeight) {
+            if (ball.y < 0 or ball.y + ball.height > screenHeight) { // bounce off floor or ceiling
                 ballYVelo *= -1.0;
             }
         }
 
-        if (ball.x + ballSize > screenWidth or ball.x < 0) {
-            if (ball.x + ballSize > screenWidth) {
+        if (ball.x + ball.width > screenWidth or ball.x < 0) { // score and reset ball
+            if (ball.x + ball.width > screenWidth) {
                 leftPlayerScore += 1;
             }
             else {
                 rightPlayerScore += 1;
             }
-
-            resetBall();
+            
+            ballInPlay = false;
+            var thread = try std.Thread.spawn(.{}, resetBall, .{});
+            thread.detach();
         }
 
+        rl.beginDrawing();
+        defer rl.endDrawing();
+        rl.clearBackground(.black);
+
+        rl.drawRectangleLines(0, 0, screenWidth, screenHeight, .blue); // world border
         rl.drawRectangleRec(leftPaddle, .white);
         rl.drawRectangleRec(rightPaddle, .white);
         rl.drawRectangleRec(ball, .white);
@@ -87,6 +92,8 @@ pub fn main() anyerror!void {
 
         str = try std.fmt.bufPrintZ(&buf, "{}", .{rightPlayerScore});
         rl.drawText(str, screenWidth / 2 + 100, screenHeight / 4, 80, .light_gray);
+
+        rand += 1.0;
     }
 }
 
@@ -119,12 +126,18 @@ fn updatePaddles() void {
 }
 
 fn resetBall() void {
-    ballInPlay = false;
     defer ballInPlay = true;
 
     ball.x = screenWidth / 2;
     ball.y = screenHeight / 2;
+    
+    // wait for a second before releasing the ball
+    var timespec: std.posix.timespec = .{ .sec = 1, .nsec = 0 };
+    _ = std.posix.system.nanosleep(&timespec, &timespec);
 
     ballXVelo *= -1.0;
 }
 
+fn lazyRand(min : f32, range : f32) f32 {
+    return @mod(rand, range) + min;
+}
